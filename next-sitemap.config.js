967a239272ca0f584d3040f52923c54f38fe55ca
miphonear <guidoaf@gmail.com>
2025-11-26 10:42:25 -0300

    26/11 x3

---
+++ next-sitemap.config.js
@@ -0,0 +1,28 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.miphone.ar',
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  // exclude: ['/admin/*', '/api/*'],
+  additionalPaths: async (config) => {
+    const today = new Date().toISOString()
+    const extra = [
+      '/', // home (opcional si ya la incluye)
+      '/apple',
+      '/android',
+      '/consolas',
+      '/wearables',
+      '/foto-video',
+      '/drone',
+      '/accesorios',
+    ]
+    return extra.map((loc) =>
+      config.transform(config, {
+        loc,
+        lastmod: today,
+        changefreq: 'daily',
+        priority: loc === '/' ? 0.7 : 0.6,
+      }),
+    )
+  },
+}
